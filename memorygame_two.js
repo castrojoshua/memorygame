ddbebced494be4b5cca35dd1801816a83dd54d9d
castrojoshua <castro.josh2507@gmail.com>
2024-05-16 21:46:06 -0400

    new files

---
+++ memorygame_two.js
@@ -0,0 +1,72 @@
+
+const emojis = [ 
+    
+"🗻",	
+"🗼",		
+"🗽",		
+"🗾",		
+"🗿",		
+"😀",		
+"😁",		
+"😂",		
+"😃",
+"😄",		
+"😅", 
+"🗻",	
+"🗼",		
+"🗽",		
+"🗾",		
+"🗿",		
+"😀",		
+"😁",		
+"😂",		
+"😃",
+"😄",		
+"😅", 
+"😀",		
+"😁",		
+"😂",		
+"😃",
+"😄",		
+"😅" 
+];
+
+
+var shuf_emojis = emojis.sort(() => (Math.random() > .5 )? 2 : -1);
+for ( var i =0; i<emojis.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+
+    box.onclick = function(){
+        this.classList.add('boxOpen')
+        setTimeout(function(){
+         if(document.querySelectorAll('.boxOpen').length > 1){
+            if(document.querySelectorAll('.boxOpen')[0].innerHTML == 
+        document.querySelectorAll('.boxOpen')[1].innerHTML){
+            document.querySelectorAll('.boxOpen')[0].classList.add
+            ('boxMatch')
+            document.querySelectorAll('.boxOpen')[1].classList.add
+            ('boxMatch')
+            document.querySelectorAll('.boxOpen')[1].classList.remove
+            ('boxMatch')
+            document.querySelectorAll('.boxOpen')[0].classList.remove
+            ('boxMatch')
+            console.log('hello')
+            if(document.querySelectorAll('.boxMatch').length == emojis.length){
+                alert('We Have A Winner!')
+    
+            }
+
+        } else {
+            document.querySelectorAll('.boxOpen')[1].classList.remove
+            ('boxOpen')
+            document.querySelectorAll('.boxOpen')[0].classList.remove
+            ('boxOpen') 
+        }
+         }
+        },500)
+    }
+
+    document.querySelector('.game').appendChild(box);
+}
